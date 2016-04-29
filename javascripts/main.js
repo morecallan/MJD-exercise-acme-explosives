00b7bdd5999b9a82bdeca8fd58f60a299899d987
Callan Morrison <callan.l.morrison@gmail.com>
2016-04-28 21:56:19 -0500

    populated dom by chaining functions to xhr loads

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,8 @@
 "use strict";
-var body = $("body")
-var output = $("#output")
+var body = $("body");
+var output = $("#output");
+var categoryDropdownOutput= $("#categoryDropdownOutput");
+var typesDropdownOutput= $("#typesDropdownOutput");
 
 //First load first XHR
 var firstXHR = function() {
@@ -41,59 +43,88 @@ var thirdXHR = function() {
 };
 
 
-var populateMainDropdown = function(data) {
-  var buildstring = `<div class="dropdown">`
+var populateMainDropdownCategories = function(data) {
+  var buildstring = `<div class="dropdown" id="categoriesDropdown">`;
   buildstring += `<button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
     Select Category
     <span class="caret"></span>
     </button>
     <ul class="dropdown-menu" id="categories" aria-labelledby="dropdownMenu1">
-    <li id=${data.categories[0].name}>${data.categories[0].name}</li>
-    <li id=${data.categories[0].name}>${data.categories[1].name}</li>
+    <li id="${data.categories[0].id}">${data.categories[0].name}</li>
+    <li id="${data.categories[1].id}">${data.categories[1].name}</li>
     </ul>
-  </div>`
-  output.append(buildstring);
-}
+  </div>`;
+  categoryDropdownOutput.html(buildstring);
+};
 
+var decideWhichDataToLoadforTypes = function(data) {
+  categoryDropdownOutput.click(function(e){
+    var categoryClicked = null;
+    if (e.target.id == 0) {
+      categoryClicked = e.target.id;
+      populateSecondaryDropDownTypes(data, categoryClicked);
+    } else if (e.target.id == 1) {
+      categoryClicked = e.target.id;
+      populateSecondaryDropDownTypes(data, categoryClicked);
+    }
+  });
+};
 
-var populateSecondaryDropDown = function(data) {
-  var buildstring = `<div class="dropdown">`
-  buildstring += `<button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-    Select Category
-    <span class="caret"></span>
-    </button>
-    <ul class="dropdown-menu" id="types"aria-labelledby="dropdownMenu1">
-    <li>${data.types[0].name}</li>
-    <li>${data.types[1].name}</li>
-    <li>${data.types[2].name}</li>
-    </ul>
-  </div>`
-  output.append(buildstring);
-}
+var populateSecondaryDropDownTypes = function(data, categoryClicked) {
+  var buildstring = `<div class="dropdown" id="typesDropdown">`;
+  buildstring += `<button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">`;
+  buildstring += `Select Type`;
+  buildstring += `<span class="caret"></span>`;
+  buildstring += `</button>`;
+  buildstring += `<ul class="dropdown-menu" id="types"aria-labelledby="dropdownMenu1">`;
+  for (var i = 0; i < data.types.length; i++) {
+    if (data.types[i].category == categoryClicked) {
+      buildstring += `<li id="${data.types[i].id}">${data.types[i].name}</li>`;
+    }
+  }
+  buildstring +=  `</ul> </div>`;
+  typesDropdownOutput.html(buildstring);
+};
 
+var decideWhichDataToLoadforProducts = function(data) {
+  typesDropdownOutput.click(function(e){
+    var typeClicked = null;
+    if (e.target.id == 0) {
+      typeClicked = e.target.id;
+      populateProductsOnDOM(data, typeClicked);
+    } else if (e.target.id == 1) {
+      typeClicked = e.target.id;
+      populateProductsOnDOM(data, typeClicked);
+    } else if (e.target.id == 2) {
+      typeClicked = e.target.id;
+      populateProductsOnDOM(data, typeClicked);
+    } else if (e.target.id == 3) {
+      typeClicked = e.target.id;
+      populateProductsOnDOM(data, typeClicked);
+    } else if (e.target.id == 4) {
+      typeClicked = e.target.id;
+      populateProductsOnDOM(data, typeClicked);
+    } else if (e.target.id == 5) {
+      typeClicked = e.target.id;
+      populateProductsOnDOM(data,typeClicked);
+    }
+  });
+};
 
-
-var populateDOM = function(data) {
+var populateProductsOnDOM = function(data, typeClicked) {
   var buildstring = "";
   for (var i = 0; i < data.products.length; i++) {
-    buildstring += `<div class="col-md-4"><div class="card">`
-    buildstring += `<h4 class="productHeader">${data.products[i].name}</h4>`
-    buildstring += `<h4 class="productDescription">${data.products[i].description}</h4>`
-    buildstring += `</div></div>`
+    if (data.products[i].type == typeClicked) {
+      buildstring += `<div class="col-md-4 card">`;
+      buildstring += `<h4 class="productHeader">${data.products[i].name}</h4>`;
+      buildstring += `<h4 class="productDescription">${data.products[i].description}</h4>`;
+      buildstring += `</div>`
+    };
   }
-    output.append("<div class=row>" + buildstring + "</div>");
-}
-
-var decideWhichDataToLoad = function() {
-  body.click(function(e){
-    console.log("e", e);
+    output.html("<div class=row>" + buildstring + "</div>");
+};
 
 
-
-
-
-  });
-}
-
-
-firstXHR().then(populateMainDropdown).then(secondXHR).then(populateSecondaryDropDown).then(decideWhichDataToLoad).then(thirdXHR).then(populateDOM);
+firstXHR().then(populateMainDropdownCategories);
+secondXHR().then(decideWhichDataToLoadforTypes);
+thirdXHR().then(decideWhichDataToLoadforProducts);
